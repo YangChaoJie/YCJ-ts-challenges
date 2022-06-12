ca87c1cabc501c0f79f5daed162a320728baadf8
YangChaoJie <1126913855@qq.com>
2022-06-12 22:18:39 +0800

    add unshift

---
+++ type-challenges/Unshift/templates.ts
@@ -0,0 +1,22 @@
+/*
+  3060 - Unshift
+  -------
+  by jiangshan (@jiangshanmeta) #easy #array
+  
+  ### Question
+  
+  Implement the type version of ```Array.unshift```
+  
+  For example
+  
+  ```typescript
+  type Result = Unshift<[1, 2], 0> // [0, 1, 2,]
+  ```
+  
+  > View on GitHub: https://tsch.js.org/3060
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+type Unshift<T extends any[], U extends any> = [U, ...T];
